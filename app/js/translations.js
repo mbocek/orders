dfffdf3db39295fcf0b85a82024cafc91461ec5c
Michal Bocek <michal.bocek@gmail.com>
2015-08-15 19:59:22 +0200

    angular upgrade, added translation

--- app/js/translations.js
+++ app/js/translations.js
@@ -1,3 +1,14 @@
 var translationsEN = {
-  TEST: 'Test EN'
+    TEST: 'Test EN',
+    // login dialog
+    SIGNIN_EMAIL: 'Email',
+    SIGNIN_PASSWORD: 'Password',
+    SIGNIN_SIGNIN: 'Signin',
+    // register dialog
+    SIGNUP_FIRSTNAME: 'First name',
+    SIGNUP_LASTNAME: 'Last name',
+    SIGNUP_EMAIL: 'Email',
+    SIGNUP_PASSWORD: 'Password',
+    SIGNUP_CONFIRM_PASSWORD: 'Confirm password',
+    SIGNUP_SIGNUP: 'Register',
 };
